bbb874762e18c2a0e30b2be2ba1060664105ac2a
katona.zsolt <katona.zsolt.mail@gmail.com>
2014-09-20 15:33:24 +0200

    new strategy

---
+++ src/main/groovy/org/leanpoker/player/NewStrategy.groovy
@@ -0,0 +1,82 @@
+package org.leanpoker.player
+
+class NewStrategy {
+
+	static enum Decision { FOLD, MAYBE_CALL, CALL, MAYBE_RAISE, RAISE }
+
+	GameHelper helper
+
+	private communityHand
+	private ourHand
+	private communityValue
+	private ourValue
+	private playerBets = []
+
+	static int raiseCount = 0
+	static int communityCardCount = 0
+
+	NewStrategy(GameHelper helper) {
+		this.helper = helper
+		communityHand = new Hand(cards: helper.communityCards)
+		communityValue = this.communityHand.handValue
+		ourHand = new Hand(cards: helper.cards)
+		ourValue = this.ourHand.handValue
+		helper.gameState.players.findAll { it.version != Player.VERSION }.each {
+			// Kati védelem
+			if (it.bet + it.stack > 0) {
+				playerBets << ((double) it.bet / (double) (it.bet + it.stack))
+			}
+		}
+		if (helper.communityCards.size() != communityCardCount) {
+			raiseCount = 0
+		}
+		communityCardCount = helper.communityCards.size()
+	}
+
+	def getDecision() {
+		def decision
+		switch (ourValue - communityValue) {
+			case 0:
+				if (communityValue in [1, 2] && communityHand.cards.size() <= 3) {
+					decision = Decision.CALL
+					break
+				}
+				decision = Decision.FOLD
+				break
+			case 1..2:
+				decision = Decision.MAYBE_CALL
+				break
+			case 3..4:
+				decision = Decision.CALL
+				break
+			case 5..6:
+				decision = Decision.MAYBE_RAISE
+				break
+			case 7..10:
+				decision = Decision.RAISE
+				break
+		}
+		switch (decision) {
+			case Decision.FOLD:
+				return 0
+			case Decision.MAYBE_CALL:
+				if (helper.currentBuyIn > helper.us.stack * 0.75 && raiseCount > 1) {
+					return 0
+				}
+			case Decision.CALL:
+				raiseCount++
+				return helper.minimumBet
+			case Decision.MAYBE_RAISE:
+				if (communityValue < 3 && playerBets.count { it > 0.75 }) {
+					return helper.minimumBet
+				}
+			case Decision.RAISE:
+				if (raiseCount < 2) {
+					raiseCount++
+					return ourValue * helper.minimumRaise
+				}
+				return helper.minimumBet
+		}
+	}
+
+}
--- src/main/groovy/org/leanpoker/player/Player.groovy
+++ src/main/groovy/org/leanpoker/player/Player.groovy
@@ -6,9 +6,9 @@ class Player {
 
 	static {
 		if (Math.random() >= 0.5) {
-			VERSION = 'groovy-random-algo'
+			VERSION = 'groovy-old-strategy'
 		} else {
-			VERSION = 'groovy-super-secret-algo-5'
+			VERSION = 'groovy-new-strategy-1'
 		}
 	}
 
@@ -16,13 +16,11 @@ class Player {
 
 		def helper = new GameHelper(gameState: gameState)
 
-		if (VERSION == 'groovy-random-algo') {
-			def random = Math.random()
-			if (random < 0.1) return 0
-			if (random < 0.6) return Math.min(helper.minimumBet + 100, helper.us.stack)
-			return helper.us.stack
-		} else {
+		if (VERSION == 'groovy-old-strategy') {
 			def strategy = new Strategy(helper)
+			return strategy.decision
+		} else {
+			def strategy = new NewStrategy(helper)
 			return strategy.decision
 		}
 	}
