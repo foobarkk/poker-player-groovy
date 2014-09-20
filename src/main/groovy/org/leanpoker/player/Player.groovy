6ee93bd77cbc43934d132936fc5c8c49d6829a39
katona.zsolt <katona.zsolt.mail@gmail.com>
2014-09-20 17:15:55 +0200

    no more random

--- src/main/groovy/org/leanpoker/player/Player.groovy
+++ src/main/groovy/org/leanpoker/player/Player.groovy
@@ -5,7 +5,7 @@ class Player {
     static String VERSION
 
 	static {
-		if (Math.random() >= 0.75) {
+		if (Math.random() >= 1.1) {
 			VERSION = 'groovy-old-strategy'
 		} else {
 			VERSION = 'groovy-new-strategy-8'
